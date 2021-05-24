8afbaa324c1d33312c3ea1f8b5d1ad2dd5f6e6b8
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-24 10:46:18 -0700

    Set state for new user creation

---
+++ client/components/Registration.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+export default function Registration() {
+
+    const initialNewUser = {
+        username: '',
+        size: ''
+    }
+
+    const [newUser, setNewUser] = useState(initialNewUser)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
