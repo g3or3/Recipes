7c91d9775ad3773693ace573fb4058d98ba51e7e
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-24 15:11:58 -0700

    Complete empty landing page

---
+++ client/src/components/EmptyLandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function EmptyLandingPage() {
+
+    return (
+        <div className='container' id='empty'>
+            <h2>You don't have any recipes yet!</h2>
+            <a href='#'>Create your first recipe</a>
+        </div>
+    )
+}
