021ec8de2d1e3abe233118736a1a50ed863530e8
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-24 11:00:14 -0700

    Build basic jsx form structure

--- client/components/Registration.jsx
+++ client/components/Registration.jsx
@@ -4,14 +4,47 @@ export default function Registration() {
 
     const initialNewUser = {
         username: '',
-        size: ''
+        password: ''
+    }
+
+    const initialFormErrors = {
+        username: '',
+        password: ''
     }
 
     const [newUser, setNewUser] = useState(initialNewUser)
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
 
     return (
         <div>
-            
+            <form className='container' id='new-user-form' onSubmit={null}>
+                <div className='inputs'>
+                    <label>Username
+                        <input
+                        type='text'
+                        name='username'
+                        value={newUser.name}
+                        onChange={null}
+                        />
+                    </label>
+                    <label>Password
+                        <input
+                        type='password'
+                        name='password'
+                        value={newUser.password}
+                        onChange={null}
+                        />
+                    </label>
+                </div>
+
+                <div className='submit'>
+                    <button disabled={null}>Submit</button>
+                    <div className='errors'>
+                        <div>{formErrors.name}</div>
+                        <div>{formErrors.password}</div>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
