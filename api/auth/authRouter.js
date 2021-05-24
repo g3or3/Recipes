f14209a90ae7c764aa7a30a7aa2b773d5d30ba65
g3or3 <>
2021-05-24 17:50:06 -0400

    testing

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -3,6 +3,10 @@ const { checkIfUserExists, validateBody } = require("./authMiddleware");
 
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+	res.json("works");
+});
+
 router.post("/register", validateBody, checkIfUserExists, authController.register);
 
 router.post("/login", validateBody, checkIfUserExists, authController.login);
