d85aed65c210a50ab7fb30f3d8840a538b28ecbd
g3or3 <>
2021-05-24 16:18:47 -0400

    test deploy

--- api/app.js
+++ api/app.js
@@ -1,19 +1,34 @@
 const path = require("path");
+const helmet = require("helmet");
 const express = require("express");
 const { restricted } = require("./auth/authMiddleware");
 
 const app = express();
 
-app.use(require("helmet")());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
+
+// app.use(
+// 	helmet.contentSecurityPolicy({
+// 		directives: {
+// 			objectSrc: ["'none'"],
+// 			styleSrc: ["self"],
+// 			defaultSrc: ["'self'"],
+//       scriptSrc: ['unsafe-inline']
+// 		},
+// 	})
+// );
+
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../../", "client/build")));
+app.use(express.static(path.join(__dirname, "../", "client/build")));
 
 app.use("/api/auth", require("./auth/authRouter"));
 app.use("/api/recipes", restricted, require("./recipes/recipesRouter"));
 
 app.use("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "../../", "client/build/index.html"));
+	res.sendFile(path.join(__dirname + "../", "client/build/index.html"));
 });
 
 app.use((err, req, res, next) => { //eslint-disable-line
