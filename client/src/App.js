eb330c52dc6fc89416fbaffbb93e589f731d287b
KrupDat2020 <mark.krupinsky@gmail.com>
2021-05-24 18:09:07 -0500

    sent pull request

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./index.css";
 
 export default function App() {
   return (
@@ -7,6 +7,12 @@ export default function App() {
       <h1>Secret Recipe's Page</h1>
       <button className="login-btn button">Log In</button>
       <button className="reg-btn button">Registration</button>
+      <div>
+        <p>
+          This page is dedicated to all of our "secret recipes". Please feel
+          free to add one you are especially fond of!
+        </p>
+      </div>
     </div>
   );
 }
