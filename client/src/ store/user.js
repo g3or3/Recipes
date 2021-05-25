9e01d2d7a7f037b64744641e12eb6c06482df488
KrupDat2020 <mark.krupinsky@gmail.com>
2021-05-25 16:51:42 -0500

    working on user auth

--- client/src/ store/user.js
+++ client/src/ store/user.js
@@ -1,15 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+import React from "react";
+import axios from "axios";
+import { axiosWithAuth } from "../auth/axiosWithAuth";
 
 const users = createSlice({
-	name: "users",
-	initialState: {
-		loading: false,
-	},
-	reducers: {
-		login: (users, action) => {
-			localStorage.setItem("token", action.payload);
-		},
-		logout: (users, action) => {},
-	},
-});
+  name: "users",
+  initialState: {
+    loading: false,
+  },
+  reducers: {
+    login: (users, action) => {
+      localStorage.setItem("token", action.payload);
+    },
+    logout: (users, action) => {
+		axiosWithAuth().post('/logout')
+       .then(res => {
+		   localStorage.removeItem('token');
+		window.location.href = '/login';
+	   })
+	.catch(err => {
+		console.log(err);
+	)}
+	}
+
 export default users.reducer;
