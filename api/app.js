89f35699e64797b8fe1f434c61877f53612f10c2
g3or3 <>
2021-05-25 14:11:39 -0400

    deploy

--- api/app.js
+++ api/app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 app.use("/api/auth", require("./auth/authRouter"));
 app.use("/api/recipes", restricted, require("./recipes/recipesRouter"));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
 		res.sendFile(path.join(__dirname + "../client/build", "index.html"));
 });
 
