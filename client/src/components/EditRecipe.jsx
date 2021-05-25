58b59be8647e430c6b1fb94f69c001ce88d07249
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-25 11:06:59 -0700

    Create initial form values

---
+++ client/src/components/EditRecipe.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function EditRecipe() {
+
+    const initialFormValues = {
+        title: {recipe.recipe_title},
+        source: {recipe.source},
+        category: {recipe.categories},
+        step: {instruction.step_number},
+        description: {instruction.description},
+        ingredientName: {ingredient.ingredient_name},
+        ingredientQuantity: {ingredient.quantity}
+    }
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
