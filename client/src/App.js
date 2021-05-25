4a2c0a2074ae26324e10a18e4dd28fa6020e20a3
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-25 13:02:04 -0700

    Add functionality to form

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom"
 import Login from "./components/Login";
 import DashboardRecipes from "./components/DashboardRecipes";
+import EditRecipe from "./components/EditRecipe";
 
 function App() {
 	return (
@@ -15,7 +16,7 @@ function App() {
 
 		{/* Protected Paths Below */}
 		<Route path="/recipes" component={DashboardRecipes} />
-		
+		<Route path="/edit-recipe" component={EditRecipe} />
 	</Switch>
 	)
 }
--- client/src/components/EditRecipe.jsx
+++ client/src/components/EditRecipe.jsx
@@ -1,60 +1,163 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+
+const initialFormValues = {
+  recipe_title: "",
+  source: "",
+  category: "",
+  step: "",
+  description: "",
+  ingredientName: "",
+  ingredientQuantity: "",
+};
 
 export default function EditRecipe() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [categories, setCategories] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [instructions, setInstructions] = useState([]);
 
-    const initialFormValues = {
-        title: {recipe.recipe_title},
-        source: {recipe.source},
-        category: {recipe.categories},
-        step: {instruction.step_number},
-        description: {instruction.description},
-        ingredientName: {ingredient.ingredient_name},
-        ingredientQuantity: {ingredient.quantity}
-    }
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-    const [formValues, setFormValues] = useState(initialFormValues)
+  const handleAddCategory = () => {
+    setCategories([...categories, formValues.category]);
+    setFormValues({ ...formValues, category: "" });
+  };
 
-    const inputChange = (name, value) => {
-        setFormValues({...formValues, [name]: value})
-    }
+  const handleAddIngredient = () => {
+    setIngredients([
+      ...ingredients,
+      {
+        ingredient_name: formValues.ingredientName,
+        quantity: formValues.ingredientQuantity,
+      },
+    ]);
+    setFormValues({
+      ...formValues,
+      ingredientName: "",
+      ingredientQuantity: "",
+    });
+  };
 
-    const onChange = evt => {
-        const {name, value} = evt.target
-        inputChange(name, value)
-    }
+  const handleAddInstruction = () => {
+    setInstructions([
+      ...instructions,
+      {
+        step_number: formValues.step,
+        description: formValues.description,
+        ingredients,
+      },
+    ]);
+    setIngredients([]);
+    setFormValues({ ...formValues, step: "", description: "" });
+  };
 
-    return (
-        <div className='recipe-edit'>
-            <form>
-                <div className='recipe-info'>
-                    <h2>Recipe Info</h2>
-                    <label>Title</label>
-                        <input
-                        type='text'
-                        name='title'
-                        value={formValues.title}
-                        onChange={onChange}
-                        />
-                    <label>Source</label>
-                        <input
-                        type='text'
-                        name='source'
-                        value={formValues.source}
-                        onChange={onChange}
-                        />
-                    <label>Category</label>
-                        <input
-                        type='text'
-                        name='category'
-                        value={formValues.category}
-                        onChange={onChange}
-                        />
-                </div>
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
 
-                <div className='instructions'>
-                    <h2>Instructions</h2>
-                </div>
-            </form>
+    const recipe = {
+      recipe_title: formValues.recipe_title,
+      source: formValues.source,
+      categories,
+      instructions,
+    };
+    console.log(recipe);
+    setFormValues(initialFormValues);
+    setCategories([]);
+    setInstructions([]);
+    setIngredients([]);
+  };
+
+  return (
+    <div className="recipe-edit">
+      <form onSubmit={handleSubmit}>
+        <div className="recipe-info">
+          <h2>Recipe Info</h2>
+          <label>
+            Title
+            <input
+              type="text"
+              name="recipe_title"
+              value={formValues.recipe_title}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Source
+            <input
+              type="text"
+              name="source"
+              value={formValues.source}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Category
+            <input
+              type="text"
+              name="category"
+              value={formValues.category}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="button" onClick={handleAddCategory}>
+            Add Category
+          </button>
         </div>
-    )
+
+        <div className="instructions">
+          <h2>Instructions</h2>
+          <div className="steps">
+            <label>
+              Step Number
+              <input
+                type="text"
+                name="step"
+                value={formValues.step}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Description
+              <input
+                type="text"
+                name="description"
+                value={formValues.description}
+                onChange={handleChange}
+              />
+            </label>
+            <div className="ingredients">
+              <label>
+                Ingredient Name
+                <input
+                  type="text"
+                  name="ingredientName"
+                  value={formValues.ingredientName}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Quantity
+                <input
+                  type="text"
+                  name="ingredientQuantity"
+                  value={formValues.ingredientQuantity}
+                  onChange={handleChange}
+                />
+              </label>
+              <button type="button" onClick={handleAddIngredient}>
+                Add Ingredient
+              </button>
+            </div>
+            <button type="button" onClick={handleAddInstruction}>
+              Add Instruction
+            </button>
+          </div>
+        </div>
+        <button>Submit Recipe</button>
+      </form>
+    </div>
+  );
 }
