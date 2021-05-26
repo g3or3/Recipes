ecb21058f9bf45da2f466b474fb364a390c77eb6
KrupDat2020 <mark.krupinsky@gmail.com>
2021-05-26 13:31:09 -0500

    working on LandingPage buttons

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 
+const handleClick = () => {};
+
 function LandingPage() {
-	return (
-		<div>
-			<h1>Secret Recipe's Page</h1>
-			<button className="login-btn button">Log In</button>
-			<button className="reg-btn button">Registration</button>
-			<div>
-				<p>
-					This page is dedicated to all of our "secret recipes". Please feel free to add
-					one you are especially fond of!
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <h1 class="home-page">Secret Recipe's Page</h1>
+      <button onClick={handleClick} className="login-btn button">
+        Log In
+      </button>
+      <button onClick={handleClick} className="reg-btn button">
+        Registration
+      </button>
+      <div>
+        <p class="home-para">
+          This page is dedicated to all of our "secret recipes". Please feel
+          free to add one you are especially fond of!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
