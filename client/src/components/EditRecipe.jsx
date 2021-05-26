7e453a8e9e6840e13bebf072a6a4edc80dfbb59d
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-26 12:59:25 -0700

    Add styled compoents to registration component and CSS reset to index.css

---
+++ client/src/components/EditRecipe.jsx
@@ -0,0 +1,194 @@
+import React, { useState } from "react";
+
+const initialList = [];
+
+const initialFormValues = {
+  recipe_title: "",
+  source: "",
+  category: "",
+  step: "",
+  description: "",
+  ingredientName: "",
+  ingredientQuantity: "",
+};
+
+export default function EditRecipe() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [categories, setCategories] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [instructions, setInstructions] = useState([]);
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleAddCategory = () => {
+    setCategories([...categories, formValues.category]);
+    setFormValues({ ...formValues, category: "" });
+  };
+
+  const handleAddIngredient = () => {
+    setIngredients([
+      ...ingredients,
+      {
+        ingredient_name: formValues.ingredientName,
+        quantity: formValues.ingredientQuantity,
+      },
+    ]);
+    setFormValues({
+      ...formValues,
+      ingredientName: "",
+      ingredientQuantity: "",
+    });
+  };
+
+  const handleAddInstruction = () => {
+    setInstructions([
+      ...instructions,
+      {
+        step_number: formValues.step,
+        description: formValues.description,
+        ingredients,
+      },
+    ]);
+    setIngredients([]);
+    setFormValues({ ...formValues, step: "", description: "" });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const recipe = {
+      recipe_title: formValues.recipe_title,
+      source: formValues.source,
+      categories,
+      instructions,
+    };
+    console.log(recipe);
+    setFormValues(initialFormValues);
+    setCategories([]);
+    setInstructions([]);
+    setIngredients([]);
+  };
+
+  return (
+    <div className="recipe-edit">
+      <form onSubmit={handleSubmit}>
+        <h2>Edit Recipe</h2>
+        <div className="recipe-info">
+          <h3>Recipe Info</h3>
+          <label>
+            Title
+            <input
+              type="text"
+              name="recipe_title"
+              value={formValues.recipe_title}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Source
+            <input
+              type="text"
+              name="source"
+              value={formValues.source}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Category
+            <input
+              type="text"
+              name="category"
+              value={formValues.category}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="button" onClick={handleAddCategory}>
+            Add Category
+          </button>
+          {categories.map((category, idx) => {
+            return <p key={idx}>{category}</p>;
+          })}
+        </div>
+
+        <div className="instructions">
+          <h3>Instructions</h3>
+          <div className="steps">
+            <label>
+              Step Number
+              <input
+                type="text"
+                name="step"
+                value={formValues.step}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Description
+              <input
+                type="text"
+                name="description"
+                value={formValues.description}
+                onChange={handleChange}
+              />
+            </label>
+            <div className="ingredients">
+              <label>
+                Ingredient Name
+                <input
+                  type="text"
+                  name="ingredientName"
+                  value={formValues.ingredientName}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Quantity
+                <input
+                  type="text"
+                  name="ingredientQuantity"
+                  value={formValues.ingredientQuantity}
+                  onChange={handleChange}
+                />
+              </label>
+              <button type="button" onClick={handleAddIngredient}>
+                Add Ingredient
+              </button>
+              {ingredients.map((ingredient, idx) => {
+                return (
+                  <p key={idx}>
+                    Ingredient: {ingredient.ingredient_name}, Quantity:{" "}
+                    {ingredient.quantity}
+                  </p>
+                );
+              })}
+            </div>
+            <button type="button" onClick={handleAddInstruction}>
+              Add Instruction
+            </button>
+            {instructions.map((instruction, idx) => {
+              return (
+                <div key={idx} className="instruction-container">
+                  <p>Step {instruction.step_number}</p>
+                  <p>{instruction.description}</p>
+                  {instruction.ingredients.map((ingredient, idx) => {
+                    return (
+                      <p key={idx}>
+                        Ingredient: {ingredient.ingredient_name}, Quantity:{" "}
+                        {ingredient.quantity}
+                      </p>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+          <div></div>
+        </div>
+        <button>Save Recipe</button>
+      </form>
+    </div>
+  );
+}
--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -4,84 +4,101 @@ import * as yup from "yup";
 import schema from "../validation/loginSchema";
 import { userRegister } from "../ store/user";
 import { useHistory } from "react-router";
+import styled from "styled-components";
+
+const StyledRegistration = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10%;
+
+  .container {
+    text-align: center;
+    padding: 2%;
+    background-color: #a7a284;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+`;
 
 const initialFormValues = {
-	username: "",
-	password: "",
+  username: "",
+  password: "",
 };
 
 const initialFormErrors = {
-	username: "",
-	password: "",
+  username: "",
+  password: "",
 };
 
 export default function Registration() {
-	const [formValues, setFormValues] = useState(initialFormValues);
-	const [formErrors, setFormErrors] = useState(initialFormErrors);
-	const [disabled, setDisabled] = useState(true);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(true);
 
-	const { push } = useHistory();
-	const { loading, registerSuccess } = useSelector((state) => state.users);
-	const dispatch = useDispatch();
+  const { push } = useHistory();
+  const { loading, registerSuccess } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
 
-	const handleChange = (evt) => {
-		const { name, value } = evt.target;
-		yup
-			.reach(schema, name)
-			.validate(value)
-			.then(() => setFormErrors({ ...formErrors, [name]: "" }))
-			.catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
-		setFormValues({ ...formValues, [name]: value });
-	};
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
+      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-	const formSubmit = (evt) => {
-		evt.preventDefault();
+  const formSubmit = (evt) => {
+    evt.preventDefault();
 
-		dispatch(userRegister(formValues));
-		setFormValues(initialFormValues);
-	};
+    dispatch(userRegister(formValues));
+    setFormValues(initialFormValues);
+  };
 
-	useEffect(() => {
-		schema.isValid(formValues).then((valid) => setDisabled(!valid));
-	}, [formValues]);
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => setDisabled(!valid));
+  }, [formValues]);
 
-	useEffect(() => {
-		if (registerSuccess) push("/login");
-	}, [registerSuccess, push]);
+  useEffect(() => {
+    if (registerSuccess) push("/login");
+  }, [registerSuccess, push]);
 
-	return (
-		<div>
-			<form className="container" id="new-user-form" onSubmit={formSubmit}>
-				<h2>Register</h2>
-				<div className="inputs">
-					<label>
-						Username
-						<input
-							type="text"
-							name="username"
-							value={formValues.username}
-							onChange={handleChange}
-						/>
-					</label>
-					<label>
-						Password
-						<input
-							type="password"
-							name="password"
-							value={formValues.password}
-							onChange={handleChange}
-						/>
-					</label>
-				</div>
+  return (
+    <StyledRegistration>
+      <form className="container" id="new-user-form" onSubmit={formSubmit}>
+        <h2>Register</h2>
+        <div className="inputs">
+          <label>
+            Username
+            <input
+              type="text"
+              name="username"
+              value={formValues.username}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              name="password"
+              value={formValues.password}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
-				<div className="submit">
-					<button disabled={disabled}>Submit</button>
-					<div className="errors">
-						<div>{formErrors.username}</div>
-						<div>{formErrors.password}</div>
-					</div>
-				</div>
-			</form>
-		</div>
-	);
+        <div className="submit">
+          <button disabled={disabled}>Submit</button>
+          <div className="errors">
+            <div>{formErrors.username}</div>
+            <div>{formErrors.password}</div>
+          </div>
+        </div>
+      </form>
+    </StyledRegistration>
+  );
 }
