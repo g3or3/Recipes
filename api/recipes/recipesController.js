e92a21c4f35a4995d1880df73a2c07cf68780901
g3or3 <>
2021-05-26 07:45:29 -0400

    added recipe tests

--- api/recipes/recipesController.js
+++ api/recipes/recipesController.js
@@ -1,6 +1,6 @@
 const Recipes = require("./recipesModel");
 
-let recipe, recipe_id, user_id;
+let recipe, user_id;
 
 const recipesController = {
 	async getAll(req, res) {
@@ -25,15 +25,18 @@ const recipesController = {
 		}
 	},
 
-	async update(req, res) {
-		res.json(await Recipes.edit());
+	async update(req, res, next) {
+		try {
+			const [updatedRecipe] = await Recipes.edit(req.recipe);
+			res.json(updatedRecipe);
+		} catch (err) {
+			next({ source: "Error while updating a recipe.", message: err });
+		}
+		res.json();
 	},
 
 	async remove(req, res) {
-		user_id = req.decoded.subject;
-		recipe_id = req.params.id;
-
-		await Recipes.drop(user_id, recipe_id);
+		await Recipes.drop(req.recipe.recipe_id);
 		res.json(req.recipe);
 	},
 };
--- api/recipes/recipesMiddleware.js
+++ api/recipes/recipesMiddleware.js
@@ -23,9 +23,13 @@ const validateBody = async (req, res, next) => {
 		req.body = await recipeSchema.validateAsync(req.body, { stripUnknown: true });
 		next();
 	} catch (err) {
+		let source;
+		if (req.method === "POST") source = "Error while creating a new recipe.";
+		else if (req.method === "PUT") source = "Error while updating a recipe.";
+
 		next({
 			status: 400,
-			source: "Error while creating a new recipe.",
+			source,
 			message: err.details[0].message,
 		});
 	}
