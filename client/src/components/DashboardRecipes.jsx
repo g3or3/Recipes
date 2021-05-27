84186e09b3a207e4fe7b2727bb30160ce380f37c
KrupDat2020 <mark.krupinsky@gmail.com>
2021-05-27 17:42:32 -0500

    added another loading spinner, still getting compile error

--- client/src/components/DashboardRecipes.jsx
+++ client/src/components/DashboardRecipes.jsx
@@ -5,56 +5,58 @@ import { fetchRecipes } from "../ store/recipe";
 import EmptyLandingPage from "./NoRecipes";
 
 export default function DashboardRecipes() {
-	const recipes = useSelector((state) => state.recipes.recipeList);
-	const dispatch = useDispatch();
-	const { push } = useHistory();
+  const recipes = useSelector((state) => state.recipes.recipeList);
+  const dispatch = useDispatch();
+  const { push } = useHistory();
 
-	useEffect(() => {
-		dispatch(fetchRecipes());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchRecipes());
+  }, [dispatch]);
 
-	const handleEdit = (id) => {
-		push(`/edit-recipe/${id}`);
-	};
+  const handleEdit = (id) => {
+    push(`/edit-recipe/${id}`);
+  };
 
-	if (!recipes.length) {
-		return <EmptyLandingPage />;
-	}
+  if (!recipes.length) {
+    return <EmptyLandingPage />;
+  }
 
-	return (
-		<>
-			<div className="recipes-container">
-				<div className="recipe-label">
-					{recipes?.map((recipe) => (
-						<div className="individual-recipe" key={recipe.recipe_id}>
-							<h2>{recipe.recipe_title}</h2>
-							<p>
-								{recipe.source}
-								{recipe.categories?.map((category) => {
-									return <p>{category}</p>;
-								})}
-								{recipe.instructions.map((instruction) => {
-									return (
-										<p>
-											{instruction.step_number}
-											{instruction.description}
-											{instruction.ingredients?.map((ingredient) => {
-												return (
-													<p>
-														{ingredient.ingredient_name}
-														{ingredient.quantity}
-													</p>
-												);
-											})}
-										</p>
-									);
-								})}
-							</p>
-							<button onClick={() => handleEdit(recipe.recipe_id)}>Edit Recipe</button>
-						</div>
-					))}
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <div class="holder">
+      <div className="recipes-container">
+        <div className="recipe-label">
+          {recipes?.map((recipe) => (
+            <div className="individual-recipe" key={recipe.recipe_id}>
+              <h2>{recipe.recipe_title}</h2>
+              <p>
+                {recipe.source}
+                {recipe.categories?.map((category) => {
+                  return <p>{category}</p>;
+                })}
+                {recipe.instructions.map((instruction) => {
+                  return (
+                    <p>
+                      {instruction.step_number}
+                      {instruction.description}
+                      {instruction.ingredients?.map((ingredient) => {
+                        return (
+                          <p>
+                            {ingredient.ingredient_name}
+                            {ingredient.quantity}
+                          </p>
+                        );
+                      })}
+                    </p>
+                  );
+                })}
+              </p>
+              <button onClick={() => handleEdit(recipe.recipe_id)}>
+                Edit Recipe
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,40 +1,55 @@
 import React from "react";
 import ReactLoading from "react-loading";
+import LoadingOverlay from "react-loading-overlay";
 
-function Loading() {
-  const [loadData, setLoadData] = useState([]);
-  const [done, setDone] = useState(undefined);
+<LoadingOverlay
+  active={isActive}
+  spinner
+  text="Loading your content..."
+></LoadingOverlay>;
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json);
-          setLoadData(json);
-          setDone(true);
-        });
-    }, 2000);
-  }, []);
-
-  return (
-    <>
-      {!done ? (
-        <ReactLoading
-          type={"bars"}
-          color={"#03fc4e"}
-          height={100}
-          width={100}
-        />
-      ) : (
-        <ul>
-          {data.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      )}
-    </>
-  );
+// function Loading() {
+//   const [loadData, setLoadData] = useState([]);
+//   const [done, setDone] = useState(undefined);
+//
+//   useEffect(() => {
+// setTimeout(() => {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+// .then((response) => response.json())
+// .then((json) => {
+//   console.log(json);
+//   setLoadData(json);
+//   setDone(true);
+// });
+// }, 2000);
+//   }, []);
+//
+//   return (
+// <>
+{
+  /* {!done ? ( */
 }
-
-export default Loading;
+// <ReactLoading
+//   type={"bars"}
+//   color={"#03fc4e"}
+//   height={100}
+//   width={100}
+// />
+//   ) : (
+// <ul>
+{
+  /* {data.map((post) => ( */
+}
+// <li key={post.id}>{post.title}</li>
+//   ))}
+{
+  /* </ul> */
+}
+//   )}
+{
+  /* </> */
+}
+//   );
+// }
+//
+// export default Loading;
