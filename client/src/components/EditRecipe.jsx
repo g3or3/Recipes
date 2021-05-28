f621d917d0afc763fdf4a08aa2d7ef47ad13652c
g3or3 <>
2021-05-28 12:51:38 -0400

    edit endpoint bug

--- client/src/components/EditRecipe.jsx
+++ client/src/components/EditRecipe.jsx
@@ -250,7 +250,7 @@ export default function EditRecipe() {
 										<div>
 											<p>Step {instruction.step_number}</p>
 											<p>{instruction.description}</p>
-											{instruction.ingredients?.map((ingredient, idx) => {
+											{instruction.ingredients.map((ingredient, idx) => {
 												return (
 													<p key={idx}>
 														Ingredient: {ingredient.ingredient_name}, Quantity:{" "}
