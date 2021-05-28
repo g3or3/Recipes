665a41aae2e6c44cf285066b214a0345d5390f25
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-28 12:55:52 -0700

    Add ThemeProvider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,16 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { ThemeProvider } from "styled-components";
+import themeObj from "./theme";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<App />
-		</Router>
-	</Provider>,
-	document.getElementById("root")
+  <Provider store={store}>
+    <Router>
+      <ThemeProvider theme={themeObj}>
+        <App />
+      </ThemeProvider>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
